3fc5e0bd13be0ed23b6500b6deaaabaf3fad515f
Jessica Brock <jessicabrock03@gmail.com>
2020-01-22 19:48:22 -0800

    debugging request in server.py

--- server.py
+++ server.py
@@ -4,34 +4,36 @@ import requests
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'seek_rhett'
+app.config["SECRET_KEY"] = "seek_rhett"
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route("/", methods=["GET", "POST"])
 def index():
     """Return homepage"""
     return render_template("index.html")
 
 
-@app.route('/results', methods=['POST', 'GET'])
+@app.route("/results", methods=["POST", "GET"])
 def results():
     """get results based on address"""
     place = request.form["place"].lower().replace(" ", "_")
     state = request.form["state"].lower()
 
-    # url = "https://api.turbovote.org/elections/upcoming?district-divisions=ocd-division/country:us/state:" + state + ",ocd-division/country:us/state:" + state + "/place:" + place
-    # print(url)
     url = "https://api.turbovote.org/elections/upcoming"
-    # qrystring = "district-divisions:ocd-division/country:us/state:" + state + ",ocd-division/country:us/state:" + state + "/place:" + place
-    qry = {"district-divisions:" + "ocd-division/country:us/state:" + state + ",ocd-division/country:us/state:" + state + "/place:" + place}
-    print(qry)
-    headers = {
-        'Accept': "application/json",
-        'cache-control': "no-cache"
-    }
+    querystring = {}
+    querystring["district-divisions"] = (
+        "ocd-division/country:us/state:"
+        + state
+        + ",ocd-division/country:us/state:"
+        + state
+        + "/place:"
+        + place
+    )
+    print(querystring)
+    headers = {"Accept": "application/json", "cache-control": "no-cache"}
 
     try:
-        response = requests.request("GET", url, headers=headers, params=qry)
+        response = requests.request("GET", url, headers=headers, params=querystring)
         print(response)
         return
     except Exception as err:
@@ -40,4 +42,4 @@ def results():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host="0.0.0.0")
