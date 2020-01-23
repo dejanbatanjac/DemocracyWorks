d7afae0fde7d727de4b5cde7261565fc51312356
Jessica Brock <jessicabrock03@gmail.com>
2020-01-22 16:05:32 -0800

    updated server.py

--- server.py
+++ server.py
@@ -18,16 +18,13 @@ def results():
     """get results based on address"""
     place = request.form["place"]
     state = request.form["state"]
-    # url = "https://api.turbovote.org/elections/upcoming?place=" + place + "&state=" + state
-    #print(url)
-    url = "https://api.turbovote.org/elections/upcoming?district-divisions=ocd-division/country:us/state:ma,ocd-division/country:us/state:ma/place:wayland
-"
-    res = requests.get(url)
-    res.headers["Content-Type"] = "json"
+
+    url = "https://api.turbovote.org/elections/upcoming?district-divisions=ocd-division/country:us/state:" + state + "/place:" + place
+    print(url)
+    headers = {'content-type': 'json'}
+    res = requests.get(url, headers=headers)
     print(res.json)
     return res.json()
-    
-
 
 
 if __name__ == "__main__":
